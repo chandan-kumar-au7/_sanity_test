8e1f4cdeef46a1b97e2fc9893537f86ab282bd86
chandan-jack-app <126096784+chandan-jack-app@users.noreply.github.com>
2023-04-26 00:49:34 +0530

    added initial project setup basic typescript boiler plate for nextjs

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+// ./pages/index.tsx
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      Populate me with Sanity Content
+    </main>
+  )
+}
